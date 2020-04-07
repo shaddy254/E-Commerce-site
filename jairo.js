7a413cabba00ca02767fda513612d8d21291e78d
shaddy254 <shedrackn0@gmail.com>
2020-04-07 04:53:54 +0300

    added files

---
+++ jairo.js
@@ -0,0 +1,38 @@
+// console.log("Jairos web page");
+// var i;
+
+// let calss23 = document.getElementsByClassName('product_information')
+// let h1style = document.getElementsByClassName('product_information')[8]
+// let add_button = document.getElementsByClassName('add_to_cart')
+// let item_price = document.getElementsByClassName('product_information')
+
+// for (let index = 0; index < add_button.length; index++) {
+//     const element = add_button[index];
+//     console.log(element)
+// }
+
+
+// console.log(item_price);
+
+// function addToCart(){
+
+// // }
+// Promise.cancellable=fn =>{
+//     var resolve,reject;
+// var p= new Promise(res,rej) =>
+// {
+//     resolve = res;
+//     reject = rej;
+//     fn(res,rej)
+// }
+// return {
+//     resolve,
+//     reject,
+//     p,
+// }
+// }
+// var{p,resolve,reject}=promise.cancellable(resolve=> selfTimer(()=>res(12),5000));
+// function
+const string = "foo"
+const string = '00'
+log(string include(substring))
